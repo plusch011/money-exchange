033512ef2c4697e3231bd4bd5340ee79b5ca52fd
Vladislav <plusch321@mail.ru>
2019-02-19 00:34:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+module.exports = function makeExchange(curr) {
+			if (curr > 10000) 
+				return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	    	let obj = {};
+	    	while (curr >= 50) {
+	    		obj.H ? obj.H++ : obj.H = 1;
+	    		curr -= 50;
+	    	}
+	    	while (curr >= 25) {
+	    		obj.Q ? obj.Q++ : obj.Q = 1;
+	    		curr -= 25;
+	    	}
+	    	while (curr >= 10) {
+	    		obj.D ? obj.D++ : obj.D = 1;
+	    		curr -= 10;
+	    	}
+	    	while (curr >= 5) {
+	    		obj.N ? obj.N++ : obj.N = 1;
+	    		curr -= 5;
+	    	}
+	    	while (curr >= 1) {
+	    		obj.P ? obj.P++ : obj.P = 1;
+	    		curr -= 1;
+	    	}
+	    	return obj;
+		}
